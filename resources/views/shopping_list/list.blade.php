acb3e2bf4a8cbd9c8dc0952417c00094568ce0de
haru <haru@example.com>
2024-06-23 07:31:10 +0000

    0623_1

--- resources/views/shopping_list/list.blade.php
+++ resources/views/shopping_list/list.blade.php
@@ -25,7 +25,7 @@
                 @endforeach
                 </div>
             @endif
-            <form action="/register" method="post">
+            <form action="/shopping_list/register" method="post">
                 @csrf
                 「買うもの」名:<input name="name" value="{{ old('name') }}"><br>
 
