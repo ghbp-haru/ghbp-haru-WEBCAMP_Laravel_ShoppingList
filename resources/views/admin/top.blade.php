501ab41af191b868d32cb32595e69134143ae413
haru <haru@example.com>
2024-07-06 08:54:09 +0000

    0706_3

--- resources/views/admin/top.blade.php
+++ resources/views/admin/top.blade.php
@@ -6,7 +6,7 @@
 @auth('admin')
         <menu label="リンク">
         <a href="/admin//top">管理画面Top</a><br>
-         <a href="/admin/user/list">ユーザー一覧</a><br>
+         <a href="/admin/user/list">ユーザ一覧</a><br>
         <a href="/admin/logout">ログアウト</a><br>
         </menu>
 @endauth
--- resources/views/user/register.blade.php
+++ resources/views/user/register.blade.php
@@ -14,8 +14,8 @@
         <form action="/user/register" method="post">
             @csrf
             名前:<input name="name" type="text"><br>
-            email：<input name="email" type="email"><br>
-            パスワード：<input  name="password" type="password"><br>
+            email:<input name="email" type="email"><br>
+            パスワード:<input  name="password" type="password"><br>
             パスワード（再度）:<input type="password" name="password_confirmation"><br>
             <button>登録する</button>
         </form>
