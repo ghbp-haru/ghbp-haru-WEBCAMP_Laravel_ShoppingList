1b8e274b20d56c24981a179b5ad5568bd175a79c
haru <haru@example.com>
2024-07-06 09:33:43 +0000

    0706_5

--- resources/views/admin/top.blade.php
+++ resources/views/admin/top.blade.php
@@ -5,7 +5,7 @@
 @section('contets')
 @auth('admin')
         <menu label="リンク">
-        <a href="/admin//top">管理画面Top</a><br>
+        <a href="/admin/top">管理画面Top</a><br>
          <a href="/admin/user/list">ユーザ一覧</a><br>
         <a href="/admin/logout">ログアウト</a><br>
         </menu>
